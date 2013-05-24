55368cc11dc3a608ff3ef804352463ca8cfdf980
piotr <piotr@piotr-945GCM-S2L.(none)>
2013-05-24 09:36:24 +0200

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Add your extension routes here
   namespace :gateway do
     match '/paybynet/:gateway_id/:order_id' => 'paybynet#show', :as => :paybynet
-    match '/paybynet/comeback/' => 'paybynet#comeback', :as => :paybynet_comeback
+    match '/paybynet/comeback/' => 'paybynet#comeback', :as => :paybynet_comeback, :via => [:get, :post]
     match '/paybynet/complete' => 'paybynet#complete', :as => :paybynet_complete
     match '/paybynet/reject' => 'paybynet#reject', :as => :paybynet_reject
     match '/paybynet/notify' => 'paybynet#notify', :as => :paybynet_notify
